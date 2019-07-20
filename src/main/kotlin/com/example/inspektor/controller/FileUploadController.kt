3b1a18fd7d21f9b4f55def522449abeedd8e0667
betterclever <paliwal.pranjal83@gmail.com>
2019-07-20 13:17:45 +0530

    Suppressed error

--- src/main/kotlin/com/example/inspektor/controller/FileUploadController.kt
+++ src/main/kotlin/com/example/inspektor/controller/FileUploadController.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
+@Suppress("SpringJavaInjectionPointsAutowiringInspection")
 @RestController
 @RequestMapping("files")
 class FileUploadController(val fileUploadService: FileUploadService) {
