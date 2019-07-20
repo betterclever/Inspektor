d24d1bc0a93285065eb6929324a1907bb425b909
betterclever <paliwal.pranjal83@gmail.com>
2019-07-20 13:36:24 +0530

    Kotlinx HTML example

--- src/main/kotlin/com/example/inspektor/controller/ObservationController.kt
+++ src/main/kotlin/com/example/inspektor/controller/ObservationController.kt
@@ -4,11 +4,12 @@ import com.example.inspektor.models.dto.ObservationCreateDto
 import com.example.inspektor.models.entities.Observation
 import com.example.inspektor.repository.ObservationRepository
 import com.example.inspektor.repository.UserRepository
+import kotlinx.html.*
+import kotlinx.html.dom.createHTMLDocument
+import kotlinx.html.dom.serialize
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/observations")
@@ -28,5 +29,30 @@ class ObservationController(
                 urgent = observationCreateDTO.urgent
         )
         return observationRepository.save(observation)
+    }
+
+    @GetMapping("/view/{id}", produces = [MediaType.TEXT_HTML_VALUE])
+    fun getObservationView(@PathVariable("id") id: Long): String {
+        val observation = observationRepository.findById(id).get()
+        return createHTMLDocument()
+                .html {
+                    body {
+                        div {
+                            h1 {
+                                +observation.title
+                            }
+                            br
+                            div {
+                                b { +"Submitted By: "}
+                                +observation.submittedBy.name
+                            }
+                            observation.photos.map {
+                                img {
+                                    src = it
+                                }
+                            }
+                        }
+                    }
+                }.serialize()
     }
 }
--- src/main/kotlin/com/example/inspektor/repository/ObservationRepository.kt
+++ src/main/kotlin/com/example/inspektor/repository/ObservationRepository.kt
@@ -2,9 +2,13 @@ package com.example.inspektor.repository
 
 import com.example.inspektor.models.entities.Observation
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
 interface ObservationRepository: JpaRepository<Observation, Long> {
 
+    @Query(value = "SELECT o from Observation o where o.submittedBy.phone = :userId")
+    fun getObservationBySubmittedBy(@Param("userId") userId: Long): List<Observation>
 }
